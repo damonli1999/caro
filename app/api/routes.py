8b6e02d889ba5708a96cbcccce6a4f29ce4d80ef
Damon Li <damonli1999@gmail.com>
2022-11-07 17:28:02 -0700

    update

--- app/api/routes.py
+++ app/api/routes.py
@@ -4,61 +4,57 @@ from models import db, User, Contact, contact_schema, contacts_schema
 
 api = Blueprint('api',__name__, url_prefix='/api')
 
-@api.route('/getdata')
-def getdata():
-    return {'yee': 'haw'}
-
-@api.route('/contacts', methods = ['POST'])
+@api.route('/cars', methods = ['POST'])
 @token_required
-def create_contact(current_user_token):
-    name = request.json['name']
-    email = request.json['email']
-    phone_number = request.json['phone_number']
-    address = request.json['address']
+def create_car(current_user_token):
+    make = request.json['make']
+    model = request.json['model']
+    year = request.json['year']
+    mileage = request.json['mileage']
     user_token = current_user_token.token
 
     print(f'BIG TESTER: {current_user_token.token}')
 
-    contact = Contact(name, email, phone_number, address, user_token = user_token )
+    car = Contact(make, model, year, mileage, user_token = user_token )
 
-    db.session.add(contact)
+    db.session.add(car)
     db.session.commit()
 
-    response = contact_schema.dump(contact)
+    response = contact_schema.dump(car)
     return jsonify(response)
 
-@api.route('/contacts', methods = ['GET'])
+@api.route('/cars', methods = ['GET'])
 @token_required
 def get_contact(current_user_token):
-    a_user = current_user_token.token
-    contacts = Contact.query.filter_by(user_token = a_user).all()
+    a_car = current_user_token.token
+    contacts = Contact.query.filter_by(user_token = a_car).all()
     response = contacts_schema.dump(contacts)
     return jsonify(response)
 
-@api.route('/contacts/<id>', methods = ['GET'])
+@api.route('/cars/<id>', methods = ['GET'])
 @token_required
-def get_single_contact(current_user_token, id):
+def get_single_car(current_user_token, id):
     contact = Contact.query.get(id)
     response = contact_schema.dump(contact)
     return jsonify(response)
 
-@api.route('/contacts/<id>', methods = ['POST','PUT'])
+@api.route('/cars/<id>', methods = ['POST','PUT'])
 @token_required
-def update_contact(current_user_token,id):
+def update_car(current_user_token,id):
     contact = Contact.query.get(id) 
-    contact.name = request.json['name']
-    contact.email = request.json['email']
-    contact.phone_number = request.json['phone_number']
-    contact.address = request.json['address']
+    contact.make = request.json['make']
+    contact.model = request.json['model']
+    contact.year = request.json['year']
+    contact.mileage = request.json['mileage']
     contact.user_token = current_user_token.token
 
     db.session.commit()
     response = contact_schema.dump(contact)
     return jsonify(response)
 
-@api.route('/contacts/<id>', methods = ['DELETE'])
+@api.route('/cars/<id>', methods = ['DELETE'])
 @token_required
-def delete_contact(current_user_token, id):
+def delete_car(current_user_token, id):
     contact = Contact.query.get(id)
     db.session.delete(contact)
     db.session.commit()
