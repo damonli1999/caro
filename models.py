a167d38620d17de22f394497afde8f5ba4c2142f
Damon Li <damonli1999@gmail.com>
2022-11-17 17:03:01 -0700

    update models

--- models.py
+++ models.py
@@ -50,18 +50,18 @@ class User(db.Model, UserMixin):
 
 class Contact(db.Model):
     id = db.Column(db.String, primary_key = True)
-    name = db.Column(db.String(150), nullable = False)
-    email = db.Column(db.String(200))
-    phone_number = db.Column(db.String(20))
-    address = db.Column(db.String(200))
+    make = db.Column(db.String(150), nullable = False)
+    model = db.Column(db.String(200))
+    year = db.Column(db.String(20))
+    mileage = db.Column(db.String(200))
     user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable = False)
 
-    def __init__(self,name,email,phone_number,address,user_token, id = ''):
+    def __init__(self,make,model,year,mileage,user_token, id = ''):
         self.id = self.set_id()
-        self.name = name
-        self.email = email
-        self.phone_number = phone_number
-        self.address = address
+        self.make = make
+        self.model = model
+        self.year = year
+        self.mileage = mileage
         self.user_token = user_token
 
 
@@ -73,7 +73,7 @@ class Contact(db.Model):
 
 class ContactSchema(ma.Schema):
     class Meta:
-        fields = ['id', 'name','email','phone_number', 'address']
+        fields = ['id', 'make','model','year', 'mileage']
 
 contact_schema = ContactSchema()
 contacts_schema = ContactSchema(many=True)
